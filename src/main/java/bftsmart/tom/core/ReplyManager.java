c28d455fd9ef1cee9e8693351740d59f7eb99fd8
Kai Anter <kai@anter.dev>
2025-02-17 16:17:47 +0100

    chore: formatting

--- src/main/java/bftsmart/tom/core/ReplyManager.java
+++ src/main/java/bftsmart/tom/core/ReplyManager.java
@@ -25,48 +25,48 @@ import org.slf4j.LoggerFactory;
  * @author joao
  */
 public class ReplyManager {
-        
+
     private LinkedList<ReplyThread> threads;
     private int iteration;
-    
+
     public ReplyManager(int numThreads, ServerCommunicationSystem cs) {
-        
+
         this.threads = new LinkedList();
         this.iteration = 0;
-        
+
         for (int i = 0; i < numThreads; i++) {
             this.threads.add(new ReplyThread(cs));
         }
-        
+
         for (ReplyThread t : threads)
             t.start();
     }
-    
+
     public void send (TOMMessage msg) {
-        
+
         iteration++;
         threads.get((iteration % threads.size())).send(msg);
 
     }
 }
 class ReplyThread extends Thread {
-        
+
     private LinkedBlockingQueue<TOMMessage> replies;
     private ServerCommunicationSystem cs = null;
-    
+
     private final Lock queueLock = new ReentrantLock();
     private final Condition notEmptyQueue = queueLock.newCondition();
-    
+
     private Map<Integer, Channel> channels;
-    
+
     ReplyThread(ServerCommunicationSystem cs) {
         this.cs = cs;
         this.replies = new LinkedBlockingQueue<TOMMessage>();
         this.channels = new HashMap<>();
     }
-    
+
     void send(TOMMessage msg) {
-        
+
         try {
             queueLock.lock();
             replies.put(msg);
@@ -76,24 +76,23 @@ class ReplyThread extends Thread {
             Logger.getLogger(ReplyThread.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public void run() {
 
 
         while (true) {
 
             try {
-                
+
                 LinkedList<TOMMessage> list = new LinkedList<>();
-                
+
                 queueLock.lock();
                 while (replies.isEmpty()) notEmptyQueue.await(10, TimeUnit.MILLISECONDS);
                 replies.drainTo(list);
                 queueLock.unlock();
-                
+
                 for (TOMMessage msg : list) {
-                    
-                    cs.getClientsConn().send(new int[] {msg.getSender()}, msg.reply, false);             
+                    cs.send(new int[] {msg.getSender()}, msg.reply);
                 }
             } catch (InterruptedException ex) {
                 LoggerFactory.getLogger(this.getClass()).error("Could not retrieve reply from queue",ex);
