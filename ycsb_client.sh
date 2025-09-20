48cd0172fc0240b5a1c3798f3a6c149583a7ce4f
Kai Anter <kai@anter.dev>
2025-09-20 21:00:33 +0200

    feat(benchmark): Add script in root to start ycsb client

---
+++ ycsb_client.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <classpath> <workload name>"
+  echo "Example: $0 isos.benchmark.ycsb.IsosYcsbClient isos_1"
+  exit 1
+fi
+
+DIR="./clients/c"
+SCRIPTNAME="client_ycsb_isos.sh"
+CLASSNAME="$1"
+WORKLOADNAME="$2"
+
+cd $DIR
+
+./$SCRIPTNAME $CLASSNAME $WORKLOADNAME
