653c566b37492ac32960ff893c61f109448bf48d
Kai Anter <kai@anter.dev>
2025-10-20 01:06:54 +0200

    feat(opt): take instead of poll in ServerConnection out

--- src/main/java/bftsmart/communication/server/ServerConnection.java
+++ src/main/java/bftsmart/communication/server/ServerConnection.java
@@ -397,7 +397,7 @@ public class ServerConnection {
       while (doWork) {
         // get a message to be sent
         try {
-          data = outQueue.poll(POLL_TIME, TimeUnit.MILLISECONDS);
+          data = outQueue.take();
         } catch (InterruptedException ex) {
           logger.error("Failed to poll message from outQueue", ex);
         }
