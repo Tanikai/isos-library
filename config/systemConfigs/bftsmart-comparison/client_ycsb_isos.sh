ed59111fd0830c7c3e72c122fd18a10b53bf9605
Kai Anter <kai@anter.dev>
2025-11-14 19:26:45 +0100

    feat(benchmark): refactor config structure for benchmarks

---
+++ config/systemConfigs/bftsmart-comparison/client_ycsb_isos.sh
@@ -0,0 +1,42 @@
+# Copyright (c) 2007-2013 Alysson Bessani, Eduardo Alchieri, Paulo Sousa, and the authors indicated in the @author tags
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#/bin/bash
+
+CLASSNAME="$1"
+WORKLOAD="$2"
+BENCHMARK_NAME="$3"
+CLIENT_ID="$4"
+
+mkdir -p "/home/ubuntu/benchmark_out/ycsb_${BENCHMARK_NAME}/"
+
+rm -f "/home/ubuntu/benchmark_out/ycsb_${BENCHMARK_NAME}/ycsb_t_${BENCHMARK_NAME}_${CLIENT_ID}.raw"
+
+# Each instance of YCSB can have up to 10.000 client IDs
+CLIENT_ID_START=$((CLIENT_ID * 10000))
+
+# -t: Run the transaction phase of the workload (the actual benchmark)
+java -Djava.security.properties="./config/java.security" \
+  -Dlogback.configurationFile="./config/logback.xml" \
+  -cp ./lib/*:./bin/ com.yahoo.ycsb.Client \
+  -t \
+  -threads 50 \
+  -P "config/ycsb_workloads/$WORKLOAD" \
+  -p measurementtype=raw \
+  -p measurement.raw.output_file="/home/ubuntu/benchmark_out/ycsb_${BENCHMARK_NAME}/ycsb_t_${BENCHMARK_NAME}_${CLIENT_ID}.raw" \
+  -p exportfile="/home/ubuntu/benchmark_out/ycsb_${BENCHMARK_NAME}/ycsb_t_${BENCHMARK_NAME}_${CLIENT_ID}.csv" \
+  -p smart-initkey="$CLIENT_ID_START" \
+  -p reportlatencyforeacherror=true \
+  -db $CLASSNAME \
+  -s
